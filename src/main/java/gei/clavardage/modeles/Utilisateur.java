828f8d2c15cbb1daa6d4e65fda634e1f447a7285
Enairo4 <orianeberry4@gmail.com>
2021-11-26 09:07:28 +0100

    Ajout classe utilisateur

---
+++ src/main/java/gei/clavardage/modeles/Utilisateur.java
@@ -0,0 +1,45 @@
+package gei.clavardage.modeles;
+
+import java.util.UUID;
+
+public class Utilisateur {
+	
+	UUID uuid;
+	String adresse;
+	private String pseudo;
+	boolean actif;
+	
+	public Utilisateur(UUID uuid, String adresse, String pseudo, boolean actif) {
+		this.uuid = uuid;
+		this.adresse = adresse;
+		this.pseudo = pseudo;
+		this.actif = actif;
+	}
+	
+	public Utilisateur(String adresse, String pseudo, boolean actif) {
+		this.uuid = UUID.randomUUID();
+		this.adresse = adresse;
+		this.pseudo = pseudo;
+		this.actif = actif;
+	}
+	
+	String getPseudo() {
+		return pseudo;
+	}
+	
+	void setPseudo(String new_pseudo) {
+		this.pseudo = new_pseudo;		
+	}
+	
+	UUID getIdentifiant () {
+		return this.uuid;
+	}
+	
+	boolean isActif() {
+		return actif;
+	}
+	
+	void setActif(boolean new_actif) {
+		this.actif = new_actif;
+	}
+}
