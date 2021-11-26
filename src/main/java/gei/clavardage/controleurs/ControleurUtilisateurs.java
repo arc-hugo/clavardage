47df4037448495a2bff262a7009ac6a80082a0fc
Enairo4 <orianeberry4@gmail.com>
2021-11-26 16:34:55 +0100

    correction controleur utilisateurs

--- src/main/java/gei/clavardage/controleurs/ControleurUtilisateurs.java
+++ src/main/java/gei/clavardage/controleurs/ControleurUtilisateurs.java
@@ -38,6 +38,12 @@ public class ControleurUtilisateurs {
 	
 	public void deconnexion () {}
 	
+	protected void receptionPseudo(UUID identifiant, String adresse, String pseudo) {
+	}
+	
+	protected void deconnexionDistante(UUID identifiant) {
+	}
+	
 	
 	
 	
