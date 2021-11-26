5fc0af886e561eec75bd8dbe60c9bf1698b19f1d
Hugo Barral <hugo.barral@outlook.fr>
2021-11-26 15:23:44 +0100

    Revert "Ajout UDP"

--- src/main/java/gei/clavardage/PrimaryController.java
+++ src/main/java/gei/clavardage/PrimaryController.java
@@ -1,10 +1,6 @@
 package gei.clavardage;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
-
-import gei.clavardage.modeles.PaquetUnicast;
-import gei.clavardage.services.ServiceEnvoiUDP;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -12,11 +8,5 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-    }
-    
-    @FXML
-    private void sendMessage() throws UnknownHostException {
-    	ServiceEnvoiUDP envoi = new ServiceEnvoiUDP(new PaquetUnicast("HELLO\n", "localhost"));
-    	envoi.start();
     }
 }
