c4e891bb58c907c3d2d5c6f6a0d7f70f5e70ce8b
Hugo Barral <hugo.barral@outlook.fr>
2021-11-30 00:00:57 +0100

    Méthodes privées et toString() utilisateur

--- src/main/java/gei/clavardage/modeles/ModeleUtilisateurs.java
+++ src/main/java/gei/clavardage/modeles/ModeleUtilisateurs.java
@@ -33,15 +33,17 @@ public class ModeleUtilisateurs {
 		utilisateurLocal.setPseudo(pseudo);
 	}
 		
-	public void connexion(Utilisateur utilisateur) {
-		UUID id = utilisateur.getIdentifiant();
-		String pseudo = utilisateur.getPseudo();
-		
-		int trouve = IntStream.range(0,utilisateurs.size())
-					.filter(u -> utilisateurs.get(u).getIdentifiant().equals(id))
-					.findFirst()
-					.orElse(-1);
+	private int getIndexById(UUID id) {
+		return IntStream.range(0,utilisateurs.size())
+				.filter(u -> utilisateurs.get(u).getIdentifiant().equals(id))
+				.findFirst()
+				.orElse(-1);
+	}
+	
+	public void connexion(UUID identifiant, String adresse, String pseudo) {
+		int trouve = getIndexById(identifiant);
 		if (trouve == -1) {
+			Utilisateur utilisateur = new Utilisateur(identifiant, adresse, pseudo, true);
 			utilisateurs.add(utilisateur);
 			utilisateur.setActif(true);
 		} else {
@@ -49,28 +51,19 @@ public class ModeleUtilisateurs {
 			util.setPseudo(pseudo);
 			util.setActif(true);
 		}
-		
 	}
 	
 	public void deconnexion(UUID identifiant) {
-		int trouve = IntStream.range(0,utilisateurs.size())
-				.filter(u -> utilisateurs.get(u).getIdentifiant().equals(identifiant))
-				.findFirst()
-				.orElse(-1);
+		int trouve = getIndexById(identifiant);
 		if (trouve >=0) {
 			Utilisateur util = utilisateurs.get(trouve);
 			util.setActif(false);
 		}
 		
-		
-		
 	}
 	
 	public void changementPseudo(UUID identifiant, String Pseudo) {
-		int trouve = IntStream.range(0,utilisateurs.size())
-				.filter(u -> utilisateurs.get(u).getIdentifiant().equals(identifiant))
-				.findFirst()
-				.orElse(-1);
+		int trouve = getIndexById(identifiant);
 		if (trouve >=0) {
 			Utilisateur util = utilisateurs.get(trouve);
 			util.setPseudo(Pseudo);
@@ -91,11 +84,15 @@ public class ModeleUtilisateurs {
 		}
 	}
 	
-	public Utilisateur getUtilisateurWithAdresse(String adresse) {
-		int trouve = IntStream.range(0,utilisateurs.size())
+	private int getIndexByAdresse(String adresse) {
+		return IntStream.range(0,utilisateurs.size())
 				.filter(u -> utilisateurs.get(u).getAdresse().equals(adresse))
 				.findFirst()
 				.orElse(-1);
+	}
+	
+	public Utilisateur getUtilisateurWithAdresse(String adresse) {
+		int trouve = getIndexByAdresse(adresse);
 		if (trouve >=0) {
 			Utilisateur util = utilisateurs.get(trouve);
 			return util;
