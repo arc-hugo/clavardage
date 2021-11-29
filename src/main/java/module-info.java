aa7a2daef0d4c7c9f018d7a984b2466aae17364a
Hugo Barral <hugo.barral@outlook.fr>
2021-11-29 22:08:05 +0100

    Ajout du package des controleurs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module gei.clavardage {
+module clavardage {
 	requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
 
     opens gei.clavardage to javafx.fxml;
+    opens gei.clavardage.controleurs to javafx.fxml;
+    
     exports gei.clavardage;
+    exports gei.clavardage.controleurs;
 }
