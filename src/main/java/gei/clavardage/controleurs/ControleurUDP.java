6d3b32a033925548eff35f2034b753b28908bef7
Hugo Barral <hugo.barral@outlook.fr>
2021-11-29 23:58:51 +0100

    Gestion des réception UDP et de la saisie de pseudo

--- src/main/java/gei/clavardage/controleurs/ControleurUDP.java
+++ src/main/java/gei/clavardage/controleurs/ControleurUDP.java
@@ -6,19 +6,18 @@ import java.util.UUID;
 import gei.clavardage.modeles.Paquet;
 import gei.clavardage.modeles.PaquetBroadcast;
 import gei.clavardage.modeles.PaquetUnicast;
-import gei.clavardage.modeles.Utilisateur;
 import gei.clavardage.services.ServiceEnvoiUDP;
 
 public class ControleurUDP {
 	
-	ControleurUtilisateurs controleurUtilisateurs;
+	ControleurUtilisateurs ctrlUtilisateurs;
 	
 	public ControleurUDP(ControleurUtilisateurs controleurUtilisateurs) {
-		this.controleurUtilisateurs = controleurUtilisateurs;
+		this.ctrlUtilisateurs = controleurUtilisateurs;
 	}
 	
 	public void pseudoLocalInvalide() {
-		//TODO Ajout notification -> ControleurUtilisateurs
+		ctrlUtilisateurs.saisiePseudo();
 	}
 	
 	private void envoi(Paquet paquet) {
@@ -27,31 +26,46 @@ public class ControleurUDP {
 		
 	}
 	
-	public void deconnexion() {
-		String msg = "DECONNEXION " 
-				+ controleurUtilisateurs.getIdentifiantLocal();
+	private void pseudoInvalide(String adresse) {
+		try {
+			String msg = "INVALIDE "
+					+ ctrlUtilisateurs.getIdentifiantLocal()+" "
+					+ctrlUtilisateurs.getPseudoLocal();
+			envoi(new PaquetUnicast(msg, adresse));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deconnexionDistante(UUID identifiant) {
+		ctrlUtilisateurs.deconnexionDistante(identifiant);
+	}
+	
+	public boolean validationUtilisateur(UUID uuid, String adresse, String pseudo) {
+		if (! ctrlUtilisateurs.validationDistante(pseudo)) {
+			pseudoInvalide(adresse);
+			return false;
+		}
+		ctrlUtilisateurs.receptionUtilisateur(uuid, adresse, pseudo);
+		String msg = "UTILISATEUR " 
+				+ ctrlUtilisateurs.getIdentifiantLocal()+" "
+				+ ctrlUtilisateurs.getPseudoLocal();
 		try {
 			envoi(new PaquetBroadcast(msg));
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+		return true;
 	}
 	
 	public void receptionUtilisateur(UUID uuid, String adresse, String pseudo) {
-		// TODO Ajout utilisateur -> ControleurUtilisateurs
-		
-		String msg = "UTILISATEUR " 
-				+ controleurUtilisateurs.getIdentifiantLocal() + " " 
-				+ controleurUtilisateurs.getPseudoLocal();
-		try {
-			envoi(new PaquetUnicast(adresse, msg));
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
+		ctrlUtilisateurs.receptionUtilisateur(uuid, adresse, pseudo);
 	}
 	
-	protected void validation(UUID uuid, String pseudo) {
-		String msg = "VALIDATION " + uuid.toString() + " " + pseudo;
+	protected void broadcastDeconnexion() {
+		String msg = "DECONNEXION " 
+				+ ctrlUtilisateurs.getIdentifiantLocal()+" "
+				+ ctrlUtilisateurs.getPseudoLocal();
 		try {
 			envoi(new PaquetBroadcast(msg));
 		} catch (UnknownHostException e) {
@@ -59,9 +73,12 @@ public class ControleurUDP {
 		}
 	}
 	
-	protected void pseudoInvalide(String adresse) {
+	protected void broadcastValidation(UUID id, String pseudo) {
+		String msg = "VALIDATION "
+				+ id.toString()+ " " 
+				+ pseudo;
 		try {
-			envoi(new PaquetUnicast("INVALIDE",adresse));
+			envoi(new PaquetBroadcast(msg));
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
